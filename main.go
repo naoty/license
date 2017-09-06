9e0304e6313ea4cd0fcc9f599d44fc9e39f017bd
Naoto Kaneko <naoty.k@gmail.com>
2017-09-06 19:49:22 +0900

    Print text in a template

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"text/template"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	path := filepath.Join("templates", "mit.txt")
+	template, err := template.ParseFiles(path)
+
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+
+	template.Execute(os.Stdout, nil)
 }
